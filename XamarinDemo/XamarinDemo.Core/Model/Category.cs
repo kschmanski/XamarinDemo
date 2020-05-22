46da8d7c03e37b03988c57bdecc39cda9815fcfc
Kaz Schmanski <kschmanski1@gmail.com>
2020-05-22 16:56:29 -0500

    initial commit

---
+++ XamarinDemo/XamarinDemo.Core/Model/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XamarinDemo.Core.Model
+{
+
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
